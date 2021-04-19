26a311a8f783a310c00b1fb380d59f6bd0f4776e
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-19 15:40:15 +0800

    Add hero component

---
+++ app/core/components/Hero.tsx
@@ -0,0 +1,59 @@
+import { Flex, Heading, Image, Stack, Text, useBreakpointValue } from "@chakra-ui/react"
+
+const Hero = () => {
+  return (
+    <Stack minH={100} direction={{ base: "column", md: "row" }} w={"full"}>
+      <Flex
+        p={8}
+        flex={1}
+        align={"center"}
+        justify={"center"}
+        paddingBottom={{ base: 0, md: 8 }}
+        marginBottom={{ base: "-2rem", md: 0 }}
+      >
+        <Stack spacing={6} w={"full"} maxW={"lg"}>
+          <Heading
+            fontSize={{ base: "4xl", md: "5xl", lg: "6xl" }}
+            lineHeight={{ base: "2.5rem", md: "3.5rem" }}
+            textAlign={{ base: "center", md: "left" }}
+          >
+            <Text as={"span"}>Awesome stuff made by us</Text>{" "}
+            <Image
+              src={"/singapore.svg"}
+              height={{ base: "30px", md: "40px" }}
+              display={"inline-block"}
+              boxShadow={"dark-lg"}
+              borderRadius={"md"}
+              ml={4}
+            />
+          </Heading>
+          <Text fontSize={{ base: "md", lg: "lg" }} textAlign={{ base: "center", md: "left" }}>
+            <Text
+              as={"span"}
+              position={"relative"}
+              _after={{
+                content: "''",
+                width: "full",
+                height: useBreakpointValue({ base: "10%", md: "20%" }),
+                position: "absolute",
+                bottom: -1,
+                left: 0,
+                bg: "red.500",
+                zIndex: -1,
+              }}
+            >
+              hyperlocal.sg
+            </Text>{" "}
+            is a listing of apps, services, products and communities made by Singaporeans. Discover
+            something new today while you <code>#supportlocal</code>!
+          </Text>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image alt={"Login Image"} objectFit={"cover"} src={"/hero.svg"} />
+      </Flex>
+    </Stack>
+  )
+}
+
+export default Hero
