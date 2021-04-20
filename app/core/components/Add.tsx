19e8c151c199e459908fbac7821e9fa09aa204a4
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-21 01:48:40 +0800

    Add modal component

---
+++ app/core/components/Add.tsx
@@ -0,0 +1,50 @@
+import { useRef } from "react"
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input,
+  ModalFooter,
+  Button,
+} from "@chakra-ui/react"
+
+const Add = (props) => {
+  const { isOpen, onOpen, onClose } = props
+
+  const initialRef = useRef()
+
+  return (
+    <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Create your account</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>
+          <FormControl>
+            <FormLabel>First name</FormLabel>
+            <Input ref={initialRef} placeholder="First name" />
+          </FormControl>
+
+          <FormControl mt={4}>
+            <FormLabel>Last name</FormLabel>
+            <Input placeholder="Last name" />
+          </FormControl>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3}>
+            Save
+          </Button>
+          <Button onClick={onClose}>Cancel</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default Add
