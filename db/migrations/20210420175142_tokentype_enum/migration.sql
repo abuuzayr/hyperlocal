aa98a1ec8e2371eb35770d89946cbc153ef2b17c
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-21 01:52:07 +0800

    Add migration files

---
+++ db/migrations/20210420175142_tokentype_enum/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Changed the type of `type` on the `Token` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- CreateEnum
+CREATE TYPE "TokenType" AS ENUM ('RESET_PASSWORD');
+
+-- AlterTable
+ALTER TABLE "Token" DROP COLUMN "type",
+ADD COLUMN     "type" "TokenType" NOT NULL;
