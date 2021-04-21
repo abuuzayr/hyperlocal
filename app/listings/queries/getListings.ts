8764269084f1f5f0b65c2ef940a1dd81e7542f8f
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-22 01:17:36 +0800

    Remove authorize resolver

--- app/listings/queries/getListings.ts
+++ app/listings/queries/getListings.ts
@@ -4,22 +4,19 @@ import db, { Prisma } from "db"
 interface GetListingsInput
   extends Pick<Prisma.ListingFindManyArgs, "where" | "orderBy" | "skip" | "take"> {}
 
-export default resolver.pipe(
-  resolver.authorize(),
-  async ({ where, orderBy, skip = 0, take = 100 }: GetListingsInput) => {
-    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const { items: listings, hasMore, nextPage, count } = await paginate({
-      skip,
-      take,
-      count: () => db.listing.count({ where }),
-      query: (paginateArgs) => db.listing.findMany({ ...paginateArgs, where, orderBy }),
-    })
+export default resolver.pipe(async ({ where, orderBy, skip = 0, take = 100 }: GetListingsInput) => {
+  // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+  const { items: listings, hasMore, nextPage, count } = await paginate({
+    skip,
+    take,
+    count: () => db.listing.count({ where }),
+    query: (paginateArgs) => db.listing.findMany({ ...paginateArgs, where, orderBy }),
+  })
 
-    return {
-      listings,
-      nextPage,
-      hasMore,
-      count,
-    }
+  return {
+    listings,
+    nextPage,
+    hasMore,
+    count,
   }
-)
+})
