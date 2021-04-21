8275d3143f673f08282e1c8d93090472467d5774
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-22 01:16:48 +0800

    Grid to make and use query

--- app/core/components/Grid.tsx
+++ app/core/components/Grid.tsx
@@ -1,105 +1,83 @@
-import { Container, Stack, Box } from "@chakra-ui/react"
+import { Suspense } from "react"
+import { useQuery, useRouter, useRouterQuery } from "blitz"
+import {
+  Container,
+  Stack,
+  Box,
+  SkeletonCircle,
+  SkeletonText,
+  Center,
+  useColorModeValue,
+} from "@chakra-ui/react"
 import Card from "app/core/components/Card"
+import getListings from "app/listings/queries/getListings"
 
-const cards = [
-  {
-    category: "products",
-    tagline: "Athetic Club Singapura",
-    img:
-      "https://static.wixstatic.com/media/6aeb45_fa4c832a0ecd4005b648f56cf2c67257~mv2.png/v1/crop/x_0,y_180,w_1080,h_721/fill/w_379,h_253,al_c,q_85,usm_0.66_1.00_0.01/2.webp",
-    logo:
-      "https://static.wixstatic.com/media/6aeb45_1759c34fbea64eff9c5bdc98df560ca8~mv2.png/v1/fill/w_63,h_95,al_c,q_85,usm_0.66_1.00_0.01/3.webp",
-    name: "We The Peepur",
-    likes: 20,
-    tags: "streetwear, atheletic",
-    website: "https://www.wethepeepur.com/",
-    social: "https://www.instagram.com/wethepeepur/",
-  },
-  {
-    category: "products",
-    tagline: "Athetic Club Singapura",
-    img:
-      "https://static.wixstatic.com/media/6aeb45_fa4c832a0ecd4005b648f56cf2c67257~mv2.png/v1/crop/x_0,y_180,w_1080,h_721/fill/w_379,h_253,al_c,q_85,usm_0.66_1.00_0.01/2.webp",
-    logo:
-      "https://static.wixstatic.com/media/6aeb45_1759c34fbea64eff9c5bdc98df560ca8~mv2.png/v1/fill/w_63,h_95,al_c,q_85,usm_0.66_1.00_0.01/3.webp",
-    name: "We The Peepur",
-    likes: 20,
-    tags: "streetwear, atheletic",
-    website: "",
-    social: "",
-  },
-  {
-    category: "products",
-    tagline: "Athetic Club Singapura",
-    img:
-      "https://static.wixstatic.com/media/6aeb45_fa4c832a0ecd4005b648f56cf2c67257~mv2.png/v1/crop/x_0,y_180,w_1080,h_721/fill/w_379,h_253,al_c,q_85,usm_0.66_1.00_0.01/2.webp",
-    logo:
-      "https://static.wixstatic.com/media/6aeb45_1759c34fbea64eff9c5bdc98df560ca8~mv2.png/v1/fill/w_63,h_95,al_c,q_85,usm_0.66_1.00_0.01/3.webp",
-    name: "We The Peepur",
-    likes: 20,
-    tags: "streetwear, atheletic",
-    website: "",
-    social: "",
-  },
-  {
-    category: "products",
-    tagline: "Athetic Club Singapura",
-    img:
-      "https://static.wixstatic.com/media/6aeb45_fa4c832a0ecd4005b648f56cf2c67257~mv2.png/v1/crop/x_0,y_180,w_1080,h_721/fill/w_379,h_253,al_c,q_85,usm_0.66_1.00_0.01/2.webp",
-    logo:
-      "https://static.wixstatic.com/media/6aeb45_1759c34fbea64eff9c5bdc98df560ca8~mv2.png/v1/fill/w_63,h_95,al_c,q_85,usm_0.66_1.00_0.01/3.webp",
-    name: "We The Peepur",
-    likes: 20,
-    tags: "streetwear, atheletic",
-    website: "",
-    social: "",
-  },
-  {
-    category: "products",
-    tagline: "Athetic Club Singapura",
-    img:
-      "https://static.wixstatic.com/media/6aeb45_fa4c832a0ecd4005b648f56cf2c67257~mv2.png/v1/crop/x_0,y_180,w_1080,h_721/fill/w_379,h_253,al_c,q_85,usm_0.66_1.00_0.01/2.webp",
-    logo: "",
-    name: "We The Peepur",
-    likes: 20,
-    tags: "streetwear, atheletic",
-    website: "https://www.wethepeepur.com/",
-    social: "https://www.instagram.com/wethepeepur/",
-  },
-  {
-    category: "services",
-    tagline: "Athetic Club Singapura",
-    img:
-      "https://static.wixstatic.com/media/6aeb45_fa4c832a0ecd4005b648f56cf2c67257~mv2.png/v1/crop/x_0,y_180,w_1080,h_721/fill/w_379,h_253,al_c,q_85,usm_0.66_1.00_0.01/2.webp",
-    logo:
-      "https://static.wixstatic.com/media/6aeb45_1759c34fbea64eff9c5bdc98df560ca8~mv2.png/v1/fill/w_63,h_95,al_c,q_85,usm_0.66_1.00_0.01/3.webp",
-    name: "We The Peepur",
-    likes: 20,
-    tags: "streetwear, atheletic",
-    website: "https://www.wethepeepur.com/",
-    social: "https://www.instagram.com/wethepeepur/",
-  },
-  {
-    category: "services",
-    tagline: "Athetic Club Singapura",
-    img: "",
-    logo:
-      "https://static.wixstatic.com/media/6aeb45_1759c34fbea64eff9c5bdc98df560ca8~mv2.png/v1/fill/w_63,h_95,al_c,q_85,usm_0.66_1.00_0.01/3.webp",
-    name: "We The Peepur",
-    likes: 20,
-    tags: "streetwear, atheletic",
-    website: "https://www.wethepeepur.com/",
-    social: "",
-  },
-]
+const GridSkeleton = () => {
+  return (
+    <>
+      {Array(...Array(12)).map((v, i) => (
+        <Center py={0} d="inline-block" maxW={"445px"} w={"full"} key={v}>
+          <Box
+            p={6}
+            overflow={"hidden"}
+            bg={"white"}
+            boxShadow={"2xl"}
+            rounded={"md"}
+            mb={4}
+            h={300}
+          >
+            <SkeletonCircle size="20" mx={"auto"} my={10} />
+            <SkeletonText mt="4" noOfLines={4} spacing="4" />
+          </Box>
+        </Center>
+      ))}
+    </>
+  )
+}
+
+const GridComponentWithQuery = (props) => {
+  const query = useRouterQuery()
+  const where = {}
+  if (query.category) {
+    where["category"] = query.category
+  }
+  if (query.search) {
+    where["OR"] = [
+      {
+        tags: {
+          contains: query.search,
+          mode: "insensitive",
+        },
+      },
+      {
+        name: {
+          contains: query.search,
+          mode: "insensitive",
+        },
+      },
+      {
+        tagline: {
+          contains: query.search,
+          mode: "insensitive",
+        },
+      },
+    ]
+  }
+  const [{ listings }] = useQuery(getListings, { where, orderBy: [], skip: 0, take: 12 })
+
+  return <>{listings && listings.map((card) => <Card data={card} />)}</>
+}
+
+const WrappedGridComponent = (props) => (
+  <Suspense fallback={<GridSkeleton />}>
+    <GridComponentWithQuery {...props} />
+  </Suspense>
+)
 
 const GridComponent = () => (
   <Container as={Stack} maxW={"7xl"} py={10}>
     <Box w="100%" mx="auto" sx={{ columnCount: [1, 2, 4], columnGap: "1.5rem" }}>
-      {/* <Grid templateColumns="repeat(4, 1fr)" gap={6} justifyContent={"start"}> */}
-      {[...cards, ...cards].map((card) => (
-        <Card data={card} />
-      ))}
+      <WrappedGridComponent />
     </Box>
   </Container>
 )
