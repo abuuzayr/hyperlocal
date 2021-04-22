134c062bd84b90140a1a683e32e3ef3a856f6938
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-22 10:39:30 +0800

    Complete listing form

--- app/listings/components/ListingForm.tsx
+++ app/listings/components/ListingForm.tsx
@@ -1,12 +1,47 @@
 import { Form, FormProps } from "app/core/components/Form"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
+import { LabeledSelectField } from "app/core/components/LabeledSelectField"
 import * as z from "zod"
-export { FORM_ERROR } from "app/core/components/Form"
+import { Button, HStack } from "@chakra-ui/react"
 
 export function ListingForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
   return (
     <Form<S> {...props}>
-      <LabeledTextField name="name" label="Name" placeholder="Name" />
+      <HStack spacing={3}>
+        <LabeledTextField name="name" label="Name" placeholder="Name" required />
+        <LabeledTextField name="namep" label="NameP" placeholder="NameP" visibility="hidden" />
+        <LabeledSelectField
+          name="category"
+          label="Category"
+          placeholder="Select Category"
+          required
+          options={["Products", "Services", "App", "Community"]}
+        />
+      </HStack>
+      <LabeledTextField
+        label="Tagline (80 characters)"
+        name="tagline"
+        placeholder="Keep it short and sweet!"
+        required
+        maxLength={80}
+      />
+      <HStack spacing={3}>
+        <LabeledTextField label="Feature image" name="img" placeholder="img" />
+        <LabeledTextField label="Logo" name="logo" placeholder="logo" />
+      </HStack>
+      <LabeledTextField
+        label="Tags"
+        name="tags"
+        placeholder="e.g. Sneakers, Music, Food"
+        helperText="Only 3 tags will be shown"
+      />
+      <HStack spacing={3}>
+        <LabeledTextField label="Website URL" name="website" placeholder="Website" />
+        <LabeledTextField label="Social Media URL" name="social" placeholder="social" />
+      </HStack>
+      <Button colorScheme="red" mr={3} type="submit" float={"left"}>
+        Add
+      </Button>
     </Form>
   )
 }
