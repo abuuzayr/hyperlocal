9d1982ff4a201462024f57e70c6e8dae1722ed01
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-22 19:48:11 +0800

    Use new image field

--- app/listings/components/ListingForm.tsx
+++ app/listings/components/ListingForm.tsx
@@ -1,8 +1,9 @@
 import { Form, FormProps } from "app/core/components/Form"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { LabeledSelectField } from "app/core/components/LabeledSelectField"
+import { LabeledImageField } from "app/core/components/LabeledImageField"
 import * as z from "zod"
-import { Button, HStack } from "@chakra-ui/react"
+import { HStack } from "@chakra-ui/react"
 
 export function ListingForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
   return (
@@ -25,9 +26,9 @@ export function ListingForm<S extends z.ZodType<any, any>>(props: FormProps<S>) 
         required
         maxLength={80}
       />
-      <HStack spacing={3}>
-        <LabeledTextField label="Feature image" name="img" placeholder="img" />
-        <LabeledTextField label="Logo" name="logo" placeholder="logo" />
+      <HStack spacing={3} w={"100%"}>
+        <LabeledImageField label="Feature image" name="img" helperText="Max size: 5MB" />
+        <LabeledImageField label="Logo" name="logo" helperText="Max size: 500KB" />
       </HStack>
       <LabeledTextField
         label="Tags"
@@ -39,9 +40,6 @@ export function ListingForm<S extends z.ZodType<any, any>>(props: FormProps<S>) 
         <LabeledTextField label="Website URL" name="website" placeholder="Website" />
         <LabeledTextField label="Social Media URL" name="social" placeholder="social" />
       </HStack>
-      <Button colorScheme="red" mr={3} type="submit" float={"left"}>
-        Add
-      </Button>
     </Form>
   )
 }
