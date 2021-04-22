d066fab35c8bd138e044c56ba579ee0285f41cf3
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-23 01:19:26 +0800

    Make listing form responsive

--- app/listings/components/ListingForm.tsx
+++ app/listings/components/ListingForm.tsx
@@ -3,12 +3,12 @@ import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { LabeledSelectField } from "app/core/components/LabeledSelectField"
 import { LabeledImageField } from "app/core/components/LabeledImageField"
 import * as z from "zod"
-import { HStack } from "@chakra-ui/react"
+import { Stack } from "@chakra-ui/react"
 
 export function ListingForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
   return (
     <Form<S> {...props}>
-      <HStack spacing={3}>
+      <Stack direction={["column", "row"]} spacing={3}>
         <LabeledTextField name="name" label="Name" placeholder="Name" required />
         <LabeledTextField name="namep" label="NameP" placeholder="NameP" visibility="hidden" />
         <LabeledSelectField
@@ -18,7 +18,7 @@ export function ListingForm<S extends z.ZodType<any, any>>(props: FormProps<S>) 
           required
           options={["Products", "Services", "App", "Community"]}
         />
-      </HStack>
+      </Stack>
       <LabeledTextField
         label="Tagline (80 characters)"
         name="tagline"
@@ -26,20 +26,20 @@ export function ListingForm<S extends z.ZodType<any, any>>(props: FormProps<S>) 
         required
         maxLength={80}
       />
-      <HStack spacing={3} w={"100%"}>
+      <Stack direction={["column", "row"]} spacing={3} w={"100%"}>
         <LabeledImageField label="Feature image" name="img" helperText="Max size: 5MB" />
         <LabeledImageField label="Logo" name="logo" helperText="Max size: 500KB" />
-      </HStack>
+      </Stack>
       <LabeledTextField
         label="Tags"
         name="tags"
         placeholder="e.g. Sneakers, Music, Food"
         helperText="Only 3 tags will be shown"
       />
-      <HStack spacing={3}>
+      <Stack direction={["column", "row"]} spacing={3}>
         <LabeledTextField label="Website URL" name="website" placeholder="Website" />
         <LabeledTextField label="Social Media URL" name="social" placeholder="social" />
-      </HStack>
+      </Stack>
     </Form>
   )
 }
