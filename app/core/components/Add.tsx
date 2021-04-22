7e1bea3c264bde51d5ffcfddd93f97c35aab3c25
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-22 10:38:09 +0800

    Complete Add listing form

--- app/core/components/Add.tsx
+++ app/core/components/Add.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react"
+import { useMutation } from "blitz"
 import {
   Modal,
   ModalOverlay,
@@ -6,42 +7,51 @@ import {
   ModalHeader,
   ModalBody,
   ModalCloseButton,
-  FormControl,
-  FormLabel,
-  Input,
-  ModalFooter,
   Button,
+  useToast,
 } from "@chakra-ui/react"
+import { ListingForm } from "app/listings/components/ListingForm"
+import createListing from "app/listings/mutations/createListing"
+import { FORM_ERROR } from "app/core/components/Form"
+import { CreateListing } from "app/auth/validations"
 
 const Add = (props) => {
   const { isOpen, onClose } = props
+  const toast = useToast()
+  const [createListingMutation] = useMutation(createListing)
 
   const initialRef = useRef<HTMLInputElement>(null)
 
   return (
-    <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+    <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose} size={"xl"}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Create your account</ModalHeader>
+        <ModalHeader>Add a new listing</ModalHeader>
         <ModalCloseButton />
         <ModalBody pb={6}>
-          <FormControl>
-            <FormLabel>First name</FormLabel>
-            <Input ref={initialRef} placeholder="First name" />
-          </FormControl>
-
-          <FormControl mt={4}>
-            <FormLabel>Last name</FormLabel>
-            <Input placeholder="Last name" />
-          </FormControl>
-        </ModalBody>
-
-        <ModalFooter>
-          <Button colorScheme="blue" mr={3}>
-            Save
+          <ListingForm
+            schema={CreateListing}
+            initialValues={{ namep: "" }}
+            onSubmit={async (values) => {
+              try {
+                await createListingMutation(values)
+                onClose()
+                toast({
+                  title: "Listing created.",
+                  description: "Thank you for your contribution!",
+                  status: "success",
+                  duration: 6000,
+                  isClosable: true,
+                })
+              } catch (error) {
+                return { [FORM_ERROR]: error.toString() }
+              }
+            }}
+          />
+          <Button onClick={onClose} mt={2}>
+            Cancel
           </Button>
-          <Button onClick={onClose}>Cancel</Button>
-        </ModalFooter>
+        </ModalBody>
       </ModalContent>
     </Modal>
   )
