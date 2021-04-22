03c8b8f50f5e6c0f2cc331c38f641aacb22e35bd
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-22 19:47:10 +0800

    Add labelled image field

---
+++ app/core/components/LabeledImageField.tsx
@@ -0,0 +1,51 @@
+import { forwardRef, PropsWithoutRef } from "react"
+import { useField } from "react-final-form"
+import { FormControl, FormLabel, Input, FormErrorMessage, FormHelperText } from "@chakra-ui/react"
+
+export interface LabeledImageFieldProps extends PropsWithoutRef<JSX.IntrinsicElements["input"]> {
+  /** Field name. */
+  name: string
+  /** Field label. */
+  label: string
+  /** Helper text. */
+  helperText?: string
+  outerProps?: PropsWithoutRef<JSX.IntrinsicElements["div"]>
+}
+
+export const LabeledImageField = forwardRef<HTMLInputElement, LabeledImageFieldProps>(
+  ({ name, label, helperText, outerProps, ...props }, ref) => {
+    const {
+      input: { value, onChange, ...input },
+      meta: { touched, error, submitError, submitting },
+    } = useField(name)
+
+    const normalizedError = Array.isArray(error) ? error.join(", ") : error || submitError
+
+    return (
+      <FormControl
+        {...outerProps}
+        isInvalid={touched && normalizedError}
+        isRequired={props.required}
+      >
+        <FormLabel fontSize="xs" mb={1}>
+          {label}
+        </FormLabel>
+        <Input
+          {...input}
+          type="file"
+          onChange={({ target }) => onChange(target.files)}
+          disabled={submitting}
+          {...props}
+          ref={ref}
+          border="none"
+          p={0}
+          borderRadius={0}
+        />
+        <FormHelperText fontSize="xs">{helperText}</FormHelperText>
+        <FormErrorMessage>{normalizedError}</FormErrorMessage>
+      </FormControl>
+    )
+  }
+)
+
+export default LabeledImageField
