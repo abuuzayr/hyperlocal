f33b8f9c2cc694d5010b6175c5d9422e887930ca
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-23 10:03:54 +0800

    Redut default take number

--- app/listings/queries/getListings.ts
+++ app/listings/queries/getListings.ts
@@ -4,7 +4,7 @@ import db, { Prisma } from "db"
 interface GetListingsInput
   extends Pick<Prisma.ListingFindManyArgs, "where" | "orderBy" | "skip" | "take"> {}
 
-export default resolver.pipe(async ({ where, orderBy, skip = 0, take = 100 }: GetListingsInput) => {
+export default resolver.pipe(async ({ where, orderBy, skip = 0, take = 25 }: GetListingsInput) => {
   // TODO: in multi-tenant app, you must add validation to ensure correct tenant
   const { items: listings, hasMore, nextPage, count } = await paginate({
     skip,
