190c3be95985bb2eb9989e7b8fdc2f3bd7b97605
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-25 11:52:41 +0800

    Fix up footer

--- app/core/components/Footer.tsx
+++ app/core/components/Footer.tsx
@@ -1,52 +1,24 @@
+import { ReactNode } from "react"
+import { Link as InternalLink } from "blitz"
 import {
+  Link,
   Box,
   chakra,
   Container,
-  Link,
   SimpleGrid,
   Stack,
   Text,
   VisuallyHidden,
   Input,
+  Icon,
   IconButton,
   useColorModeValue,
   Image,
 } from "@chakra-ui/react"
-import { ReactNode } from "react"
-import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa"
+import { FiGithub, FiShare2 } from "react-icons/fi"
 import { BiMailSend } from "react-icons/bi"
-
-const SocialButton = ({
-  children,
-  label,
-  href,
-}: {
-  children: ReactNode
-  label: string
-  href: string
-}) => {
-  return (
-    <chakra.button
-      bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
-      rounded={"full"}
-      w={8}
-      h={8}
-      cursor={"pointer"}
-      as={"a"}
-      href={href}
-      display={"inline-flex"}
-      alignItems={"center"}
-      justifyContent={"center"}
-      transition={"background 0.3s ease"}
-      _hover={{
-        bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
-      }}
-    >
-      <VisuallyHidden>{label}</VisuallyHidden>
-      {children}
-    </chakra.button>
-  )
-}
+import { BsLightningFill } from "react-icons/bs"
+import { IoStatsChart } from "react-icons/io5"
 
 const ListHeader = ({ children }: { children: ReactNode }) => {
   return (
@@ -68,34 +40,66 @@ const Footer = () => {
             <Box>
               <Image src={"/logo_word.png"} w={200} />
             </Box>
-            <Text fontSize={"sm"}>© {new Date().getFullYear()} hyperlocal.sg</Text>
             <Stack direction={"row"} spacing={6}>
-              <SocialButton label={"Twitter"} href={"#"}>
-                <FaTwitter />
-              </SocialButton>
-              <SocialButton label={"YouTube"} href={"#"}>
-                <FaYoutube />
-              </SocialButton>
-              <SocialButton label={"Instagram"} href={"#"}>
-                <FaInstagram />
-              </SocialButton>
+              <Link href="https://github.com/abuuzayr/hyperlocal" target="_blank">
+                <a>
+                  <IconButton
+                    size={"sm"}
+                    aria-label={"GitHub repository"}
+                    icon={<FiGithub size={18} />}
+                  />
+                </a>
+              </Link>
+              <IconButton
+                d={["block", "none"]}
+                size={"sm"}
+                aria-label={"Share"}
+                icon={<FiShare2 size={18} />}
+                onClick={() => {
+                  try {
+                    // @ts-ignore
+                    navigator.share({
+                      title: "hyperlocal.sg - Awesome stuff made by Singaporeans",
+                      url: "https://hyperlocal.sg",
+                    })
+                  } catch (e) {}
+                }}
+              />
+              <Link href="https://app.panelbear.com/share/104rkj82wxFYqkfp64s6fb/" target="_blank">
+                <a>
+                  <IconButton
+                    size={"sm"}
+                    aria-label={"Site statistics"}
+                    icon={<IoStatsChart size={18} />}
+                  />
+                </a>
+              </Link>
             </Stack>
           </Stack>
           <Stack align={"flex-start"}>
-            <ListHeader>Company</ListHeader>
-            <Link href={"#"}>About us</Link>
-            <Link href={"#"}>Blog</Link>
-            <Link href={"#"}>Contact us</Link>
-            <Link href={"#"}>Pricing</Link>
-            <Link href={"#"}>Testimonials</Link>
+            <ListHeader>About</ListHeader>
+            <InternalLink href={"/why"}>Why</InternalLink>
+            <Link target="_blank" href={"#"}>
+              Founder Stories<Text fontSize="sm"> - coming soon!</Text>
+            </Link>
           </Stack>
           <Stack align={"flex-start"}>
             <ListHeader>Support</ListHeader>
-            <Link href={"#"}>Help Center</Link>
-            <Link href={"#"}>Terms of Service</Link>
-            <Link href={"#"}>Legal</Link>
-            <Link href={"#"}>Privacy Policy</Link>
-            <Link href={"#"}>Satus</Link>
+            <InternalLink href={"/?add=listing"} scroll={false}>
+              Add a listing
+            </InternalLink>
+            <Link target="_blank" href={"#"}>
+              Edit a listing
+            </Link>
+            <Link target="_blank" href={"#"}>
+              Delete a listing
+            </Link>
+            <Link target="_blank" href={"https://github.com/abuuzayr/hyperlocal/issues"}>
+              Issues
+            </Link>
+            <Link target="_blank" href={"https://github.com/abuuzayr/hyperlocal"}>
+              Contribute
+            </Link>
           </Stack>
           <Stack align={"flex-start"}>
             <ListHeader>Stay up to date</ListHeader>
@@ -118,6 +122,22 @@ const Footer = () => {
                 icon={<BiMailSend />}
               />
             </Stack>
+            <Text fontSize={"xs"} mt={3}>
+              No spam, we promise
+            </Text>
+            <Text fontSize={"xs"} mt={3}>
+              Built with{" "}
+              <Link href="https://blitzjs.com/" rel="nofollow noopener">
+                <a>
+                  <Icon as={BsLightningFill} boxSize={5} color="gold" d={"inline-block"} />
+                </a>
+              </Link>{" "}
+              by{" "}
+              <Link href="https://github.com/abuuzayr" textDecor="underline">
+                <a>@abuuzayr</a>
+              </Link>{" "}
+              © {new Date().getFullYear()} hyperlocal.sg
+            </Text>
           </Stack>
         </SimpleGrid>
       </Container>
