5cdd62d9af19b3a436714cf96641f3a977529c46
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-27 07:36:57 +0800

    Add ListingModal to handle create and edit

---
+++ app/core/components/ListingModal.tsx
@@ -0,0 +1,71 @@
+import { useRef, Suspense } from "react"
+import { useRouter } from "blitz"
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useToast,
+  Spinner,
+  HStack,
+} from "@chakra-ui/react"
+import Add from "app/core/components/Add"
+import Edit from "app/core/components/Edit"
+
+const ListingModal = (props) => {
+  const { isOpen, onClose } = props
+  const toast = useToast()
+  const router = useRouter()
+  const initialRef = useRef<HTMLInputElement>(null)
+
+  const CREATE = router.query?.add && router.query?.add === "listing"
+  const UPDATE = router.query?.edit && parseInt(router.query?.edit as string)
+
+  const onCloseAndClear = () => {
+    onClose()
+    if (router.query?.add || router.query?.edit) {
+      router.push("/", undefined, { shallow: true })
+    }
+  }
+
+  return (
+    <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onCloseAndClear} size={"xl"}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalCloseButton position="absolute" right={3} top={3} />
+        <ModalHeader>{CREATE ? "Add a new listing" : "Edit listing"}</ModalHeader>
+        <ModalBody pb={6}>
+          {CREATE && <Add {...props} toast={toast} onCloseAndClear={onCloseAndClear} />}
+          {UPDATE && UPDATE > 0 && (
+            <WrappedEdit
+              {...props}
+              toast={toast}
+              onCloseAndClear={onCloseAndClear}
+              listingId={UPDATE}
+            />
+          )}
+          <Button onClick={onCloseAndClear} mt={2}>
+            Cancel
+          </Button>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+const WrappedEdit = (props) => (
+  <Suspense
+    fallback={
+      <HStack w={"100%"}>
+        <Spinner mx="auto" />
+      </HStack>
+    }
+  >
+    <Edit {...props} />
+  </Suspense>
+)
+
+export default ListingModal
