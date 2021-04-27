8e0aa0d63486cd754654d09c07e111bbf6784f8f
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-27 12:02:07 +0800

    No need name for homepage

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -35,6 +35,6 @@ const Home: BlitzPage = () => {
 }
 
 Home.suppressFirstRenderFlicker = true
-Home.getLayout = (page) => <Layout title="Home">{page}</Layout>
+Home.getLayout = (page) => <Layout>{page}</Layout>
 
 export default Home
