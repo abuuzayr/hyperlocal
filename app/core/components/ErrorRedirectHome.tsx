34a112b2bf847f9b8a101c602b8b79545e97dafc
Abu Uzayr <abu.uzayr@builtforfifty.com>
2021-04-28 10:50:52 +0800

    Reload the page if we need to redirect

--- app/core/components/ErrorRedirectHome.tsx
+++ app/core/components/ErrorRedirectHome.tsx
@@ -5,9 +5,19 @@ import Error from "app/core/components/Error"
 const ErrorRedirectHome = ({ statusCode, title }) => {
   const router = useRouter()
   useEffect(() => {
-    if (router.query && !router.query.retry) router.push("/?retry=1")
+    if (router.query && !router.query.retry) {
+      window.location.replace("/?retry=1")
+    }
   }, [])
-  return <Error statusCode={statusCode} title={title}></Error>
+  return (
+    <Error
+      statusCode={statusCode}
+      title={title}
+      text={
+        router.query && !router.query.retry ? "Hang on, we're reloading the page for you.." : ""
+      }
+    ></Error>
+  )
 }
 
 export default ErrorRedirectHome
